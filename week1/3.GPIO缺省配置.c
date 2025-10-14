430954072a81352fcb0c8892285eeae0a60e1fe9
a2917622891 <2917622891@qq.com>
2025-10-14 21:41:50 +0800

    Add files via upload

---
+++ week1/3.GPIO缺省配置.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+typedef enum {
+    GPIO_Speed_2MHz, 
+    GPIO_Speed_10MHz,
+    GPIO_Speed_50MHz 
+} GPIO_SpeedTypeDef;
+
+typedef struct {
+    GPIO_SpeedTypeDef GPIO_Speed; 
+} GPIO_InitTypeDef;
+
+void GPIO_StructureInit(GPIO_InitTypeDef* GPIO_InitStruct) {
+    if (GPIO_InitStruct != NULL) {
+        GPIO_InitStruct->GPIO_Speed = GPIO_Speed_2MHz;
+    }
+}
+
+int main() {
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    GPIO_StructureInit(&GPIO_InitStructure);
+ printf("默认的GPIO速度为: %d\n", GPIO_InitStructure.GPIO_Speed);
+    
+    return 0;
+}
+    
