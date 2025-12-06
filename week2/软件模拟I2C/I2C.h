0f2e89295f388b912920cc1b61bd7989ec2a76f4
a2917622891 <2917622891@qq.com>
2025-12-06 14:36:05 +0800

    Update I2C.h

--- week2/软件模拟I2C/I2C.h
+++ week2/软件模拟I2C/I2C.h
@@ -1,12 +1,13 @@
-#ifndef __I2C_H__  
+#ifndef __I2C_H__
 #define __I2C_H__
-#include "RM.h"
 
+#include "RM.h"  // 直接引用RM.h，获取SDA/SCL相关函数
 
-void I2C_Start(void); 
-void I2C_Stop(void);
-void I2C_SendByte(unsigned char dat);
-unsigned char I2C_ReceiveAck(void); 
-void I2C_SendData(unsigned char dat); 
+// 函数声明（
+void I2C_Start(void);        // 起始条件
+void I2C_Stop(void);         // 终止条件
+void I2C_SendByte(unsigned char dat);  // 发送一个字节（8Bit）
+unsigned char I2C_ReceiveAck(void);    // 接收应答
+void I2C_SendData(unsigned char dat);  // 完整流程：Start→SendByte→ReceiveAck→Stop
 
 #endif
